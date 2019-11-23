262517e2141debe4a74508ad8143114657ee06a9
Sam <super8989@gmail.com>
2019-11-23 16:55:23 -0500

    mapping over colors in Palette.js to display background color and name in ColorBox component

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import ColorBox from "./ColorBox";
 
 class Palette extends Component {
 	render() {
+		const colorBoxes = this.props.colors.map(color => (
+			<ColorBox background={color.color} name={color.name} />
+		));
 		return (
 			<div className='Palette'>
 				{/* Navbar goes here  */}
-				<div className='Palette-colors'>{/* color boxes */}</div>
+				<div className='Palette-colors'>{colorBoxes}</div>
 				{/* footer */}
 			</div>
 		);
