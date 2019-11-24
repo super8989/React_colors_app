7bbeadb818f17401983cb0fc083d293500d7709c
Sam <super8989@gmail.com>
2019-11-24 14:08:46 -0500

    copy-overlay grows to full screen when clicked

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -17,11 +17,15 @@ class ColorBox extends Component {
 
 	render() {
 		const { name, background } = this.props;
+		const { copied } = this.state;
 
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background }} className='ColorBox'>
-					<div style={{ background }} className='copy-overlay' />
+					<div
+						style={{ background }}
+						className={`copy-overlay ${copied && "show"}`}
+					/>
 					<div className='copy-container'>
 						<div className='box-content'>
 							<span>{name}</span>
