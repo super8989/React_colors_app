dbc58de948d3cf84be6b4335ecad0c0d83416016
Sam <super8989@gmail.com>
2019-11-25 20:40:36 -0500

    installed rc-slider to show in Palette.js

--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 class Palette extends Component {
 	render() {
@@ -9,6 +11,7 @@ class Palette extends Component {
 		));
 		return (
 			<div className='Palette'>
+				<Slider />
 				{/* Navbar goes here  */}
 				<div className='Palette-colors'>{colorBoxes}</div>
 				{/* footer */}
