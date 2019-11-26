2d331cafa2ffc1624cc89c23985b112b3894b3be
Sam <super8989@gmail.com>
2019-11-25 20:56:10 -0500

    completed the unstyled slider to change shade based on level

--- src/Palette.js
+++ src/Palette.js
@@ -8,15 +8,30 @@ class Palette extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+	}
+
+	changeLevel(level) {
+		console.log(level);
+		this.setState({ level });
 	}
 
 	render() {
-		const colorBoxes = this.props.palette.colors[
-			this.state.level
-		].map(color => <ColorBox background={color.hex} name={color.name} />);
+		const { colors } = this.props.palette;
+		const { level } = this.state;
+
+		const colorBoxes = colors[level].map(color => (
+			<ColorBox background={color.hex} name={color.name} />
+		));
 		return (
 			<div className='Palette'>
-				<Slider defaultValue={this.state.level} min={100} max={900} />
+				<Slider
+					defaultValue={level}
+					min={100}
+					max={900}
+					step={100}
+					onAfterChange={this.changeLevel}
+				/>
 				{/* Navbar goes here  */}
 				<div className='Palette-colors'>{colorBoxes}</div>
 				{/* footer */}
