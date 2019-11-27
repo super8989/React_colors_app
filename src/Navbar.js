df99149fbbf91e5ea6dd568efe0890d0b389ecdc
Sam <super8989@gmail.com>
2019-11-26 20:21:58 -0500

    created a Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+	render() {
+		return (
+			<header className='Navbar'>
+				<div className='logo'>
+					<a href='#'>Color Picker</a>
+				</div>
+			</header>
+		);
+	}
+}
+
+export default Navbar;
