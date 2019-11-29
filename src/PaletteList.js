f0c2048a15ea6a58c6e3bd861704c98047b80907
Sam <super8989@gmail.com>
2019-11-28 21:47:42 -0500

    created PaletteList and rendered a list of palette names

---
+++ src/PaletteList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class PaletteList extends Component {
+	render() {
+		const { palettes } = this.props;
+
+		return (
+			<div>
+				<h1>React Colors</h1>
+				{palettes.map(palette => (
+					<h1>{palette.paletteName}</h1>
+				))}
+			</div>
+		);
+	}
+}
+
+export default PaletteList;
