ffe0498d8246c9c8d766063791f6cbd67dd406f1
Sam <super8989@gmail.com>
2019-11-28 22:19:22 -0500

    writing CSS in JS using withStyles demo

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+	main: {
+		backgroundColor: "purple",
+		border: "3px solid yellow"
+	},
+	secondary: {
+		backgroundColor: "pink",
+		"& h1": {
+			color: "white",
+			"& span": {
+				backgroundColor: "yellow"
+			}
+		}
+	}
+};
 
 function MiniPalette(props) {
+	const { classes } = props;
+	console.log(classes);
+
 	return (
-		<div>
+		<div className={classes.main}>
 			<h1>Mini Palette</h1>
+			<section className={classes.secondary}>
+				<h1>
+					Mini Palette secondary <span>inside h1</span>
+				</h1>
+				<span>outside h1</span>
+			</section>
 		</div>
 	);
 }
 
-export default MiniPalette;
+export default withStyles(styles)(MiniPalette);
