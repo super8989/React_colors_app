0aa1428b05a0f910a0979fd7442edd767551d688
Sam <super8989@gmail.com>
2019-12-05 21:32:08 -0500

    refactored goBack button on SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import { withStyles } from "@material-ui/styles";
+import chroma from "chroma-js";
 
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
@@ -15,6 +16,36 @@ const styles = {
 	},
 	colors: {
 		height: "90%"
+	},
+	goBack: {
+		width: "20%",
+		height: "50%",
+		margin: "0 auto",
+		display: "inline-block",
+		position: "relative",
+		cursor: "pointer",
+		marginBottom: "-3.5px",
+		opacity: 1,
+		backgroundColor: "black",
+		"& a": {
+			color: "white",
+			width: "100px",
+			height: "30px",
+			position: "absolute",
+			display: "inline-block",
+			top: "50%",
+			left: "50%",
+			marginLeft: "-50px",
+			marginTop: "-15px",
+			textAlign: "center",
+			outline: "none",
+			background: "rgba(255, 255, 255, 0.3)",
+			fontSize: "1rem",
+			lineHeight: "30px",
+			textTransform: "uppercase",
+			border: "none",
+			textDecoration: "none"
+		}
 	}
 };
 
@@ -63,10 +94,8 @@ class SingleColorPalette extends Component {
 				<Navbar handleChange={this.changeFormat} showingAllColors={false} />
 				<div className={classes.colors}>
 					{colorBoxes}
-					<div className='go-back ColorBox'>
-						<Link to={`/palette/${id}`} className='back-button'>
-							Go Back
-						</Link>
+					<div className={classes.goBack}>
+						<Link to={`/palette/${id}`}>Go Back</Link>
 					</div>
 				</div>
 				<PaletteFooter paletteName={paletteName} emoji={emoji} />
