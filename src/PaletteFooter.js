58f8584202a818c3b1f66bc3ec9bc1e82821396e
Sam <super8989@gmail.com>
2019-12-05 23:18:55 -0500

    moved over styles to PaletteFooterStyles.js

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,22 +1,8 @@
 import React from "react";
 
+import styles from "./styles/PaletteFooterStyles";
+
 import { withStyles } from "@material-ui/styles";
-
-const styles = {
-	PaletteFooter: {
-		backgroundColor: "white",
-		height: "5vh",
-		display: "flex",
-		justifyContent: "flex-end",
-		alignItems: "center",
-		fontWeight: "bold"
-	},
-
-	emoji: {
-		fontSize: "1.5rem",
-		margin: "0 0.5rem"
-	}
-};
 
 function PaletteFooter(props) {
 	const { paletteName, emoji, classes } = props;
