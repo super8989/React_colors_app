88616ceb11cf767fe746de74108a1e89aaca5fe1
Sam <super8989@gmail.com>
2019-12-12 23:24:21 -0500

    created DraggableColorList component

---
+++ src/DraggableColorList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { SortableContainer } from "react-sortable-hoc";
+
+function DraggableColorList() {
+	return <div>hello</div>;
+}
+
+export default DraggableColorList;
