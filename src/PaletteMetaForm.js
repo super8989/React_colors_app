531f8c18ec6dc5fa3c3774a46fb26cb5d654d73e
Sam <super8989@gmail.com>
2019-12-17 23:03:38 -0500

    refactored TextValidator in PaletteMetaForm

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -58,39 +58,40 @@ class PaletteMetaForm extends Component {
 					onClose={this.handleClose}
 					aria-labelledby='form-dialog-title'
 				>
-					<DialogTitle id='form-dialog-title'>Subscribe</DialogTitle>
-					<DialogContent>
-						<DialogContentText>
-							To subscribe to this website, please enter your email address
-							here. We will send updates occasionally.
-						</DialogContentText>
-						<ValidatorForm
-							onSubmit={() => this.props.handleSubmit(newPaletteName)}
-						>
+					<DialogTitle id='form-dialog-title'>
+						Choose a Palette Name
+					</DialogTitle>
+					<ValidatorForm
+						onSubmit={() => this.props.handleSubmit(newPaletteName)}
+					>
+						<DialogContent>
+							<DialogContentText>
+								Please enter a name for your new palette. Make sure it's unique!
+							</DialogContentText>
+
 							<TextValidator
 								label='Palette Name'
 								value={newPaletteName}
 								name='newPaletteName'
 								onChange={this.handleChange}
+								fullWidth
+								margin='normal'
 								validators={["required", "isPaletteNameUnique"]}
 								errorMessages={[
 									"Enter Palette name",
 									"Palette name is already taken"
 								]}
 							/>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={this.handleClose} color='primary'>
+								Cancel
+							</Button>
 							<Button variant='contained' color='primary' type='submit'>
 								Save Palette
 							</Button>
-						</ValidatorForm>
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={this.handleClose} color='primary'>
-							Cancel
-						</Button>
-						<Button onClick={this.handleClose} color='primary'>
-							Subscribe
-						</Button>
-					</DialogActions>
+						</DialogActions>
+					</ValidatorForm>
 				</Dialog>
 			</div>
 		);
