fa7963ec344173baed10dbbe20f8c219b6ebe22f
Sam <super8989@gmail.com>
2019-12-17 21:23:49 -0500

    added style to ColorPicker container

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -51,7 +51,16 @@ const styles = theme => ({
 			duration: theme.transitions.duration.enteringScreen
 		}),
 		marginLeft: 0
-	}
+	},
+	container: {
+		width: "90%",
+		height: "100%",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	buttons: {}
 });
 
 class NewPaletteForm extends Component {
@@ -156,29 +165,31 @@ class NewPaletteForm extends Component {
 						</IconButton>
 					</div>
 					<Divider />
-					<Typography variant='h4'>Design Your Palette</Typography>
-					<div>
-						<Button
-							variant='contained'
-							color='secondary'
-							onClick={this.clearColors}
-						>
-							Clear Palette
-						</Button>
-						<Button
-							variant='contained'
-							color='primary'
-							onClick={this.addRandomColor}
-							disabled={paletteIsFull}
-						>
-							Random Colour
-						</Button>
+					<div className={classes.container}>
+						<Typography variant='h4'>Design Your Palette</Typography>
+						<div className={classes.buttons}>
+							<Button
+								variant='contained'
+								color='secondary'
+								onClick={this.clearColors}
+							>
+								Clear Palette
+							</Button>
+							<Button
+								variant='contained'
+								color='primary'
+								onClick={this.addRandomColor}
+								disabled={paletteIsFull}
+							>
+								Random Colour
+							</Button>
+						</div>
+						<ColorPickerForm
+							paletteIsFull={paletteIsFull}
+							addNewColor={this.addNewColor}
+							colors={colors}
+						/>
 					</div>
-					<ColorPickerForm
-						paletteIsFull={paletteIsFull}
-						addNewColor={this.addNewColor}
-						colors={colors}
-					/>
 				</Drawer>
 				<main
 					className={classNames(classes.content, {
