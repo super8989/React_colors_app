d252e3c1c672dcbca15c3d1ed9e15e635ba8f943
Sam <super8989@gmail.com>
2019-12-18 23:30:43 -0500

    moved styles out and addeadded constants.js

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -6,22 +6,7 @@ import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
 import { ChromePicker } from "react-color";
 
-const styles = {
-	picker: {
-		width: "100% !important",
-		marginTop: "2rem"
-	},
-	addColor: {
-		width: "100%",
-		padding: "1rem",
-		marginTop: "1rem",
-		fontSize: "1.5rem"
-	},
-	colorNameInput: {
-		width: "100%",
-		height: "70px"
-	}
-};
+import styles from "./styles/ColorPickerFormStyles";
 
 class ColorPickerForm extends Component {
 	constructor(props) {
