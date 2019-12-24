be33f9e304fbb00d6a0b61a804e3160624138d90
Sam <super8989@gmail.com>
2019-12-23 22:46:55 -0500

    added classNames to ColorBox.js to refactor className on div

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,6 +4,8 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
 import styles from "./styles/ColorBoxStyles";
 
+import classNames from "classnames";
+
 import { withStyles } from "@material-ui/styles";
 
 class ColorBox extends Component {
@@ -34,9 +36,14 @@ class ColorBox extends Component {
 				<div style={{ background }} className={classes.ColorBox}>
 					<div
 						style={{ background }}
-						className={`${classes.copyOverlay} ${copied &&
-							classes.showOverlay}`}
+						className={classNames(classes.copyOverlay, {
+							[classes.showOverlay]: copied
+						})}
 					/>
+
+					{/* {`${classes.copyOverlay} ${copied &&
+							classes.showOverlay}`}					 */}
+
 					<div
 						className={`${classes.copyMessage} ${copied &&
 							classes.showMessage}`}
