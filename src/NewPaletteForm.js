1bca375b7f1da912a92b1c0289b56c56db2faaf5
Sam <super8989@gmail.com>
2019-12-24 20:47:28 -0500

    fixed a bug when creating a new palette after all palettes have been deleted

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,6 +10,7 @@ import IconButton from "@material-ui/core/IconButton";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
 
+import seedColors from "./seedColors";
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
 import DraggableColorList from "./DraggableColorList";
@@ -25,7 +26,7 @@ class NewPaletteForm extends Component {
 		super(props);
 		this.state = {
 			open: true,
-			colors: this.props.palettes[0].colors
+			colors: seedColors[0].colors
 		};
 		this.addNewColor = this.addNewColor.bind(this);
 		this.handleChange = this.handleChange.bind(this);
